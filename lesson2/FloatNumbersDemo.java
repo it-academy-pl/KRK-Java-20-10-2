fd8b032047c651e486576b6431cec9e2b8b9991c
olegmiercoles <48553027+olegmiercoles@users.noreply.github.com>
2020-10-07 20:30:13 +0200

    lesson 2 demo files

---
+++ lesson2/FloatNumbersDemo.java
@@ -0,0 +1,15 @@
+public class FloatNumbersDemo {
+	public static void main(String[] args) {
+		double a = 0.0;
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+		a = a + 0.1;
+		System.out.println(a);
+	}
+}
---
+++ lesson2/NumbersDemo.java
@@ -0,0 +1,7 @@
+public class NumbersDemo {
+	public static void main(String[] args) {
+		int firstNumber = 42;
+		int secondNumber = 100;
+		System.out.println(firstNumber + secondNumber);
+	}
+}
