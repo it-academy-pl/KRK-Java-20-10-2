495b5370d1baf64e3ac52716c15d4efb16e9d83e
olegmiercoles <oleg.miercoles@dot.com>
2020-11-28 17:08:48 +0100

    Banking template

--- src/pl/itacademy/bank/BankingApplication.java
+++ src/pl/itacademy/bank/BankingApplication.java
@@ -1,7 +1,18 @@
 package pl.itacademy.bank;
 
 public class BankingApplication {
+
+    static {
+        initBanking();
+    }
+
     public static void main(String[] args) {
 
+    }
+
+    private static void initBanking() {
+        // create accounts
+        // create banks with accounts
+        // register banks in NationalBank
     }
 }
